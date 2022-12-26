1ac70386a5611e9ff14952cc65704234e89536c6
Od Tselmuun <odtselmuun@Ods-MacBook-Pro.local>
2022-12-26 16:31:28 +0100

    service locater

--- src/main/java/web/pages/ServiceLocater.java
+++ src/main/java/web/pages/ServiceLocater.java
@@ -12,15 +12,7 @@ public class ServiceLocater {
         Properties jndiProperties=new Properties();
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
         jndiProperties.put(Context.PROVIDER_URL,"http-remoting://127.0.0.1:8080");
-        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-        jndiProperties.put("jboss.naming.client.ejb.context", true);
-        jndiProperties.put("endpoint.name", "client-endpoint");
-        jndiProperties.put("remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLED", false);
-        jndiProperties.put("remote.connections", "default");
-        jndiProperties.put("remote.connection.default.connect.options.org.xnio.Options.SASL_POLICY_NOANONYMOUS", false);
 
-        jndiProperties.put(Context.SECURITY_PRINCIPAL, "user");
-        jndiProperties.put(Context.SECURITY_CREDENTIALS, "user");
         Context ctx=null;
         StudentStateless bean=null;
         try {
